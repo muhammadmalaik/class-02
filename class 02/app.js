f30630e857a27c0cb3b48d5aa776704605fd617f
muhammadmalaik <muhammadmalaik76@gmail.com>
2024-09-11 11:59:07 -0700

    Add files via upload

---
+++ class 02/app.js
@@ -0,0 +1,14 @@
+var sum = 10;
+
+console.log("before value ==>",sum);
+
+var update = --sum;
+
+console.log("after value ==>",sum);
+
+
+var a = 10 , b = 2
+
+var exp = a++ + b-- - --a;
+
+console.log(exp)
